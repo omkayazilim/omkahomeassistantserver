b9d994b87467f0ea1a62ed61987e4fc8c2c92c6c
Orhan Güneş <orhangune@gmail.com>
2024-03-25 01:48:14 +0300

    fix channel operations

---
+++ espserver/Migrations/20240323235424_deviceicon.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace espserver.Migrations
+{
+    /// <inheritdoc />
+    public partial class deviceicon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DeviceChannelIcon",
+                table: "DeviceChannelDef",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeviceChannelIcon",
+                table: "DeviceChannelDef");
+        }
+    }
+}
