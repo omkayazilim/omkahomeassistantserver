e796e96ac74369c27238e8329c50786b82557dff
Orhan Güneş <orhangune@gmail.com>
2024-10-08 00:09:11 +0300

    version up

--- espserver/Controllers/UtilController.cs
+++ espserver/Controllers/UtilController.cs
@@ -9,7 +9,7 @@ namespace espserver.Controllers
         [HttpGet]
         public IActionResult Version()
         {
-            return Ok("Api Version :V0.05");
+            return Ok("Api Version :V0.06");
         }
     }
 }
